a80fb036950beeddf2732d2c33c46ae183c51ca0
Marcela Aguirre <aguirremarcela@hotmail.com.ar>
2021-12-08 14:27:01 -0300

    publicacion del usuario

---
+++ TP5/EntregaFinal/publicar.js
@@ -0,0 +1,9 @@
+let boton4=document.getElementById("boton4");
+boton4.addEventListener('click', ()=> {
+    boton4.style.backgroundColor="#889090";
+});
+
+function cambiar(){
+    boton4.style.backgroundColor="#307a7a";
+    
+}
